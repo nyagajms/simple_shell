f1687d41f5ca2a102febe90828c25a2d2ddc7bdb
nyaga <jamesnyagakanyago@gmail.com>
2023-09-14 13:27:37 +0300

    task_1_first_command_line/executer_function_file.c

--- task_1_first_command_line/executer_function_file.c
+++ task_1_first_command_line/executer_function_file.c
@@ -4,3 +4,23 @@
  * execution - file that enables the command to be executed
  * @text: text command inputed by the user
 */
+void execution(const char *text)
+{
+	__pid_t second_pid = fork();
+	if (second_pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (second_pid == 0)
+	{
+		execve(text,text,(char *)NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);		
+	}
+	else
+	{
+		wait(NULL);
+	}
+	
+}
