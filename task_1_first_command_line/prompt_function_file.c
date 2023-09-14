697a9887e4c5233be2365ac9c720e92d9dcb2cb3
nyaga <jamesnyagakanyago@gmail.com>
2023-09-14 12:14:58 +0300

    prompt file

--- task_1_first_command_line/prompt_function_file.c
+++ task_1_first_command_line/prompt_function_file.c
@@ -1,1 +1,14 @@
 #include"mjsh_main.h"
+
+/**
+ * prompt - function that dispalys the prompt
+ * 
+*/
+
+void prompt(void)
+{
+    for (;;)
+    {
+        _printf("┌──moreen@james: \n └─$");
+    }
+}
