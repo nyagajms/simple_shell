b79dc43282aaf9116a8451f93dec0ffc925dc013
Nyarango <moreennyarango0@gmail.com>
2023-09-19 14:14:35 -0700

    mjsh.c

--- simple_shell_0.1/mjsh.c
+++ simple_shell_0.1/mjsh.c
@@ -1,7 +1,11 @@
 #include"mjsh_main.h"
 /**
- * main - main shell file
- * Return: 0
+ * main - Entry point for the main shell program
+ *
+ * This function serves as the main entry point for the shell program
+ * it runs an infinite loop that repeatedly prompts for user input and reads a progress instructions
+ *
+ * Return: Always 0 for sucess
 */
 
 int main (void)
