fab442d3f1a5786e7fa3ff7e336a33dd25fc7843
nyaga <jamesnyagakanyago@gmail.com>
2023-09-20 01:35:20 +0300

    new file:   simple_shell_0.4/PATH_function.c new file:   simple_shell_0.4/exit_function_file.c

---
+++ simple_shell_0.4/Tokenizer_function_file.c
@@ -0,0 +1,18 @@
+#include"mjsh_main.h"
+void tokenizeInput(char *trimmed_text, char *args[], int *arg_count) 
+{
+    
+    char *token;
+    
+    *arg_count = 0;
+
+    token = strtok(trimmed_text, " "); 
+    
+    while (token != NULL) 
+    {
+        args[(*arg_count)++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[*arg_count] = NULL;
+
+}
---
+++ simple_shell_0.4/exit_function_file.c
@@ -0,0 +1,7 @@
+#include"mjsh_main.h"
+
+void exitShell() 
+{
+    _printf("Exiting the shell...\n");
+    exit(0);
+}
