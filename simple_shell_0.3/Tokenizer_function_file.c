fa4b3e6164a7afc26a1e2ad31089216b40755a14
Nyarango <moreennyarango0@gmail.com>
2023-09-19 15:29:35 -0700

    Tokenizer_function_file.c

--- simple_shell_0.3/Tokenizer_function_file.c
+++ simple_shell_0.3/Tokenizer_function_file.c
@@ -1,4 +1,10 @@
 #include"mjsh_main.h"
+/**
+ * tokenizInput - tokeniz a string into an array of arguments
+ * takes 'trimmed_text' and splits it into tokens based on space (' ')
+ * it populates an array 'args' with these tokens and updates 'args_count' to indicate
+ * the number of tokens
+ */
 void tokenizeInput(char *trimmed_text, char *args[], int *arg_count) 
 {
     
