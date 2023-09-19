4346ca97d702368677afd4b7e3d46dc49825674c
Nyarango <moreennyarango0@gmail.com>
2023-09-19 14:33:03 -0700

    mjsh.c

--- simple_shell_0.2/mjsh.c
+++ simple_shell_0.2/mjsh.c
@@ -1,7 +1,11 @@
 #include"mjsh_main.h"
 /**
  * main - main shell file
- * Return: 0
+ * this is the main entry point that sets up infinite loop to continously
+ * prompt the user for commands and process those commands.
+ * it runs till the user exits.
+ *
+ * Return: Always 0 to indicate sucess
 */
 
 int main (void)
