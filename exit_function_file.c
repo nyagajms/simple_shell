97cac90baaad91639c1ad43e2ff88adec79228d7
nyaga <jamesnyagakanyago@gmail.com>
2023-09-23 11:09:50 +0300

    still trying ooo

--- exit_function_file.c
+++ exit_function_file.c
@@ -1,10 +1,10 @@
-#include"mjsh_main.h"
+#include "mjsh_main.h"
 
 /**
- * exitShell - for exiting the shell on call
-*/
-
-void exitShell(void)
+ * exitShell - for exiting the shell on call with a specific status code
+ * @status_code: the exit status code
+ */
+void exitShell(int status_code)
 {
-	exit(0);
+    exit(status_code);
 }
