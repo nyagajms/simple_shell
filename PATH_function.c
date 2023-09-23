0f090bc1fa57f4a294976a0c47e91b07d7825341
nyaga <jamesnyagakanyago@gmail.com>
2023-09-23 12:31:56 +0300

    still not yet

--- PATH_function.c
+++ PATH_function.c
@@ -15,56 +15,22 @@ void checkCommandInPath(char *trimmed_text) {
     int arg_count;
 
     tokenizeInput(trimmed_text, args, &arg_count);
- 
 
-    if (args[0] != NULL) 
-    {
-        int i;
-        int has_directory_path = 0;
-        for (i = 1; i < arg_count; i++) 
-        {
-            if (args[i][0] == '/') 
-            {
-                has_directory_path = 1;
-                break;
-            }
-        }
-
-        if (has_directory_path) 
-        {
-            
-            char *directory_path = args[i];
-            struct stat st;
-            if (stat(directory_path, &st) == 0 && S_ISDIR(st.st_mode)) 
-            {
-                args[arg_count] = NULL;  
-                chdir(directory_path);
-            } else 
-            {
-                
-                fprintf(stderr, "Error: Not a directory: %s\n", directory_path);
-                return;
-            }
-        }
-
+    if (args[0] != NULL) {
         child_pid = fork();
 
-        if (child_pid == -1) 
-        {
+        if (child_pid == -1) {
             perror("Child process failed to be created");
             exit(1);
         }
 
-        if (child_pid == 0) 
-        {
+        if (child_pid == 0) {
             if (execvp(args[0], args) == -1) {
                 perror("execvp");
                 exit(1);
             }
             exit(0);
-        } 
-        else 
-        {
+        } else {
             wait(NULL);
         }
     } else {
