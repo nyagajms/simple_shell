990c78cd337b140a6585a7549d1c78980d1687cd
nyaga <jamesnyagakanyago@gmail.com>
2023-09-23 12:25:22 +0300

    restoring alittle

--- PATH_function.c
+++ PATH_function.c
@@ -1,5 +1,4 @@
 #include "mjsh_main.h"
-
 /**
  * checkCommandInPath - search for and execute a command in the system's PATH.
  * This function checks whether the given 'command_line' represents an absolute or relative
@@ -7,33 +6,67 @@
  * If not, it searches for the command in the listed directories in the PATH environment variables.
  * If a matching executable is found, it is executed as a child process.
  */
-
 void checkCommandInPath(char *trimmed_text) {
     pid_t child_pid;
-
     char *args[10];
     int arg_count;
 
     tokenizeInput(trimmed_text, args, &arg_count);
 
-    if (args[0] != NULL) {
+
+    if (args[0] != NULL) 
+    {
+        int i;
+        int has_directory_path = 0;
+        for (i = 1; i < arg_count; i++) 
+        {
+            if (args[i][0] == '/') 
+            {
+                has_directory_path = 1;
+                break;
+            }
+        }
+
+        if (has_directory_path) 
+        {
+
+            char *directory_path = args[i];
+            struct stat st;
+            if (stat(directory_path, &st) == 0 && S_ISDIR(st.st_mode)) 
+            {
+                args[arg_count] = NULL;  
+                chdir(directory_path);
+            } else 
+            {
+
+                fprintf(stderr, "Error: Not a directory: %s\n", directory_path);
+                return;
+            }
+        }
+
         child_pid = fork();
 
-        if (child_pid == -1) {
+        if (child_pid == -1) 
+        {
             perror("Child process failed to be created");
             exit(1);
         }
 
-        if (child_pid == 0) {
+        if (child_pid == 0) 
+        {
             if (execvp(args[0], args) == -1) {
                 perror("execvp");
                 exit(1);
             }
             exit(0);
-        } else {
+        } 
+        else 
+        {
             wait(NULL);
         }
     } else {
+
+
         perror("Command not found");
     }
 }
